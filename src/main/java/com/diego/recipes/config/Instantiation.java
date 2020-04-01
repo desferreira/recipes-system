1e85604c5dfa85ad8d476756ed049d545fe13b4a
desferreira <diego.ferreira@justa.com.vc>
2020-04-01 07:37:41 -0300

    :art: Altera arquivo de configuração para YAML

--- src/main/java/com/diego/recipes/config/Instantiation.java
+++ src/main/java/com/diego/recipes/config/Instantiation.java
@@ -7,7 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Configuration
 public class Instantiation implements CommandLineRunner {
